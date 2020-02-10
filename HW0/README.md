e5259a80e0c91218cc9f1b4df2397e6a043d5246
Sarah Strand <39971563+16strands@users.noreply.github.com>
2020-02-10 12:36:19 -0800

    Finished assignment

--- HW0/README.md
+++ HW0/README.md
@@ -1,4 +1,6 @@
-# Problem 1
+## HW0 - Sarah Strand
+
+# Part 1
 a. I chose the number of iterations each time based on whether running that many objects with that many iterations 3 times would stabilize, down to 0.01us. 
   * For 2^8-2^11 I chose 10000 iterations 
   * For 2^12-2^13 I chose 1000
@@ -7,5 +9,18 @@ a. I chose the number of iterations each time based on whether running that many
   * For 2^20-2^24 I chose 5
   
  b. Since I ran each of them three times to test stability, I selected the middle of the three numbers as my final answer.
+
+ c. See graph images in the repo.
  
- 
+ # Problem 2
+* I used floats as my default type for the C++ version
+* I've included the bash script I used to call the program repeatedly and pipe the output to a log file in comma separated value format. I then copied and pasted that info into a google sheet and created the graphs. There are 2-3 data points per num_objects. I removed outliers as I saw fit. 
+* I decided on the num_iterations for each num_objects based on time constraints. I figured that including all three measured times on the scatterplot and creating a trend line would help give a good approximation of actual time. 
+* I noticed a roughly 45 speedup from python to C++. See the graphs included in the repo. 
+
+# Problem 3
+* I was running low on time to spend on this assignment so I prioritized testing float, double, and double. I tried testing int8_t, but I couldn't figure out how to get around data loss / type conversion issues in the time I had left. 
+* I saw that double and float were roughly the same speed (fastest), int64_t was in the middle, and python was the slowest. 
+
+# Problem 4
+* Didn't have time to work on this one. 
--- HW0/main.cc
+++ HW0/main.cc
@@ -1,3 +1,5 @@
+// HW0 by Sarah Strand
+
 #include <array>
 #include <random>
 #include <iostream>
@@ -5,19 +7,20 @@
 #include <numeric>
 #include <string>
 
+// Generate random list
 float *generate_random_list(int size, float bound)
 {
-
-    float *ret = new float[size];
-    std::mt19937 generator(size);
+    float *ret = new float[size]; // Initialize list 
+    std::mt19937 generator(size); // Set up random number generator (with size as seed)
     std::uniform_real_distribution<> distribution(-bound, bound);
     for (int n = 0; n < size; n++)
     {
-        ret[n] = distribution(generator);
+        ret[n] = distribution(generator); // Replace default vals with random nums
     };
     return ret;
 }
 
+// Update coordinates 
 void update_coords(float *xs, float *ys, float *zs, float *vx, float *vy, float *vz, int size)
 {
     for (int i=0; i<size; i++)
@@ -28,27 +31,34 @@ void update_coords(float *xs, float *ys, float *zs, float *vx, float *vy, float 
     }
 }
 
+
+// Main 
 int main(int argc, char *argv[])
 {
-    if (argc < 3){
+    // Check for correct number of args, convert to usable types 
+    if (argc < 3){ 
         std::cout << "Required arguments : vector_length(N) and iterations_num(M)" << std::endl;
         return 1;
     };
     int size = std::stoi(argv[1]);
     int iterations_num = std::stoi(argv[2]);
 
-    float *xs = generate_random_list(size, 1000.0);
-    float *ys = generate_random_list(size, 1000.0);
-    float *zs = generate_random_list(size, 1000.0);
-    float *vx = generate_random_list(size, 1.0);
-    float *vy = generate_random_list(size, 1.0);
-    float *vz = generate_random_list(size, 1.0);
+    // Generate the lists 
+    float *xs = generate_random_list(size, 1000);
+    float *ys = generate_random_list(size, 1000);
+    float *zs = generate_random_list(size, 1000);
+    float *vx = generate_random_list(size, 1);
+    float *vy = generate_random_list(size, 1);
+    float *vz = generate_random_list(size, 1);
 
+    // Set up timer
     auto t1 = std::chrono::high_resolution_clock::now();
+    // Update coords iterations_num number of times
     for (int i=0; i<iterations_num; i++)
     {
         update_coords(xs, ys, zs, vx, vy, vz, size);
     };
+    // Final time measurement, calculate checksum, calculate time for each object 
     auto t2 = std::chrono::high_resolution_clock::now();
     int chksum = 0;
     chksum = std::accumulate(xs, xs+size, chksum);
@@ -56,9 +66,13 @@ int main(int argc, char *argv[])
     chksum = std::accumulate(zs, zs+size, chksum);
     auto time = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
     double time2 = (double) time / (double) (size * iterations_num);
-    // std::cout << "Mean time per coordinate : " << time2 << "us" << std::endl;
-    // std::cout << "Final checksum: " << std::to_string(chksum) << std::endl;
-    std::cout << size << ", " << time2 << ", " << std::to_string(chksum)<< std::endl;
+
+    // For pretty printing result to terminal 
+    std::cout << "Mean time per coordinate : " << time2 << "us" << std::endl;
+    std::cout << "Final checksum: " << std::to_string(chksum) << std::endl;
+    
+    // For csv printing to terminal (designed to be piped to log file using >>)
+    // std::cout << size << ", " << time2 << ", " << std::to_string(chksum)<< std::endl;
 
     return 0;
 };
