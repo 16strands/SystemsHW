ffff77fdb6b425980a53851b1f190049f5b48af3
flastest <pleasantbowel5yndrome@gmail.com>
2020-03-31 19:44:17 -0700

    made server class

--- HW4/cache_server.cc
+++ HW4/cache_server.cc
@@ -4,8 +4,71 @@
 #include <string>
 #include <iostream>
 #include <memory>
+#include "cache.hh"
+
+using key_type = std::string;
+using val_type = std::string;
+
 
 namespace po = boost::program_options;
+
+class Cache_Server
+{
+public:
+
+    
+    //GET should take a key type and return a value type
+    val_type GET(key_type key) 
+    {
+        return "";
+    }
+
+    //PUT puts a value in a key
+    //sets in the cache 
+    void PUT(key_type key, val_type value)
+    { //from eitan's in class example, when he went to s.e.rv.e.r:PORT/key, it showed the value
+        //this puts it in the internet
+
+        //we somehow need to put the vlaue in subdirectory "key" of our server and port
+    }
+
+    void DELETE(key_value key)
+    {
+        //deletes the key
+    }
+
+    //this returns just the head, no body
+    std::pair<int, std::string> HEAD()
+    {
+        return std::pair<int,std::string>(0,"");
+    }
+
+    //resets the cache?
+    void POST(std::string argument)
+    {
+        //if the argument isn't reset, then do nothing
+        if(argument == reset) {
+            cache.reset();
+        }
+    }
+
+
+    //we need to listen to that port. 
+    //figure out how to listen to ports
+
+    //then we need to parse what we're hearing this should be a for loop or something, listening and parsing
+    // what we're hearing might be in http, so we have to parse that?
+
+    //determine what to do at some point, define what happens with get and put
+    // need a cache to modify
+
+};
+
+
+
+
+
+
 
 int main(int argc, char** argv){
 
@@ -31,6 +94,7 @@ int main(int argc, char** argv){
             ("threads,t", po::value<int>(& threads)->default_value(1),
                     "number of threads")
             ;
+
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
@@ -47,6 +111,4 @@ int main(int argc, char** argv){
             ioc,
             tcp::endpoint{address, port},
             doc_root)->run();
-
-
 }
