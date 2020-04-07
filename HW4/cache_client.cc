186c1f82a299b3b83f07bf03cb6bd6e6403b4e18
flastest <pleasantbowel5yndrome@gmail.com>
2020-04-07 14:04:58 -0700

    added return values

--- HW4/cache_client.cc
+++ HW4/cache_client.cc
@@ -50,23 +50,6 @@ public:
         stream.connect(results);
     }
 
-    void sendRequest(http::request<http::empty_body> req){
-        // Send the HTTP request to the remote host
-        http::write(stream, req);
-
-        // This buffer is used for reading and must be persisted
-        beast::flat_buffer buffer;
-
-        // Declare a container to hold the response
-        http::response<http::dynamic_body> res;
-
-        // Receive the HTTP response
-        http::read(stream, buffer, res);
-
-        // Write the message to standard out
-        std::cout << res << std::endl;
-    }
-
     void set(key_type key, Cache::val_type val, Cache::size_type size)
     {
         http::request<http::empty_body> req;
@@ -79,10 +62,20 @@ public:
 
         (void) size;
 
-        sendRequest(req);
+        http::write(stream, req);
+
+        // This buffer is used for reading and must be persisted
+        beast::flat_buffer buffer;
+
+        // Declare a container to hold the response
+        http::response<http::dynamic_body> res;
+
+        // Receive the HTTP response
+        http::read(stream, buffer, res);
+
     }
 
-    void get(key_type key, Cache::size_type& val_size)
+    std::string get(key_type key, Cache::size_type& val_size)
     {
         http::request<http::empty_body> req;
         req.version(11);
@@ -93,10 +86,24 @@ public:
 
         (void) val_size;
 
-        sendRequest(req);
+        http::write(stream, req);
+
+        // This buffer is used for reading and must be persisted
+        beast::flat_buffer buffer;
+
+        // Declare a container to hold the response
+        http::response<http::dynamic_body> res;
+
+        // Receive the HTTP response
+        http::read(stream, buffer, res);
+
+        
+        // Write the message to standard out
+        std::string value = res.body().substr(res.body().find(":")+2,res.body().size()-3);
+        return value;
     }
 
-    void del(key_type key)
+    bool del(key_type key)
     {
         http::request<http::empty_body> req;
         req.version(11);
@@ -105,10 +112,21 @@ public:
         req.set(http::field::host, host_);
         req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-        sendRequest(req);
+        http::write(stream, req);
+
+        // This buffer is used for reading and must be persisted
+        beast::flat_buffer buffer;
+
+        // Declare a container to hold the response
+        http::response<http::dynamic_body> res;
+
+        // Receive the HTTP response
+        http::read(stream, buffer, res);
+
+        return true;
     }
 
-    void space_used()
+    Cache::size_type space_used()
     {
         http::request<http::empty_body> req;
         req.version(11);
@@ -116,7 +134,19 @@ public:
         req.set(http::field::host, host_);
         req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-        sendRequest(req);
+        http::write(stream, req);
+
+        // This buffer is used for reading and must be persisted
+        beast::flat_buffer buffer;
+
+        // Declare a container to hold the response
+        http::response<http::dynamic_body> res;
+
+        // Receive the HTTP response
+        http::read(stream, buffer, res);
+
+        return std::stoi( std::string(res["space_used"]));
+        
     }
 
     // Reset will set the mCache variable to a new unordered map, writing over the other one
@@ -129,7 +159,16 @@ public:
         req.set(http::field::host, host_);
         req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-        sendRequest(req);
+        http::write(stream, req);
+
+        // This buffer is used for reading and must be persisted
+        beast::flat_buffer buffer;
+
+        // Declare a container to hold the response
+        http::response<http::dynamic_body> res;
+
+        // Receive the HTTP response
+        http::read(stream, buffer, res);
     }
 
 };
