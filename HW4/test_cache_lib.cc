50c552aaedcc60df3d49bdb998a088d01593b49b
flastest <pleasantbowel5yndrome@gmail.com>
2020-04-09 19:11:27 -0700

    changed asserts to print statements

--- HW4/test_cache_lib.cc
+++ HW4/test_cache_lib.cc
@@ -189,6 +189,7 @@ int main(){
     if (testEvictorEvictingSameItemTwice == (false))std::cout << "test evictingsameitemtwice failed"<<std::endl;
 
 
+
     std::cout<<"all tests ran!"<<std::endl;
     return 0;
 }
